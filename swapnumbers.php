4db41c204f5ad2a43c7b940cd450d0f51052683c
Ankush <ankush357159@gmail.com>
2022-03-18 17:48:11 +0530

    Swap Numbers

---
+++ swapnumbers.php
@@ -0,0 +1,22 @@
+<?php 
+$a = 45;
+$b = 50;
+
+echo "Numbers before swap: <br>";
+echo "a=".$a."  b=".$b;
+echo "<br><br>";
+
+//Swapping logic
+$temp = $a;
+$a = $b;
+$b = $temp;
+
+
+echo "Numbers after swap: <br>";
+echo "a=".$a. "  b=".$b;
+
+
+
+?>
+
+
