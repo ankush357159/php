6e0320efc10cea8d8adf291ad7ea12c242cbc61b
Ankush <ankush357159@gmail.com>
2022-03-20 15:56:20 +0530

    Alphabet triangular patterns

---
+++ alphabet-triangle-pattern.php
@@ -0,0 +1,19 @@
+<!-- <?php  
+$alpha = range('A', 'Z');  
+for($i=0; $i<5; $i++){   
+  for($j=0; $j<=$i; $j++){  
+    echo $alpha[$j];  
+    }  
+    echo "<br>";  
+}  
+?>   -->
+
+<?php  
+$alpha = range('A', 'Z');  
+for($i=0; $i<5; $i++){   
+  for($j=0; $j<=$i; $j++){  
+    echo $alpha[$i];  
+    }  
+    echo "<br>";  
+}  
+?>  
