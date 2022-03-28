641c7843824106bceb6a9be6d6787bc51915a63a
Ankush <ankush357159@gmail.com>
2022-03-28 12:01:00 +0530

    Associative Array

---
+++ associative-array.php
@@ -0,0 +1,7 @@
+<?php 
+$salary = array("Ajay"=>"10000", "Vijay"=>"15000", "Sanjay"=>"12000");
+echo "Ajay Salary".$salary["Ajay"]."<br>";
+echo "Vijay Salary".$salary["Vijay"]."<br>";
+echo "Sanjay Salary".$salary["Sanjay"]."<br>";
+
+?>
