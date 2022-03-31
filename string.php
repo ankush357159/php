8af559951018de7af4480e1e4e93c18d0b0eefb8
Ankush <ankush357159@gmail.com>
2022-04-01 00:39:17 +0530

    String in php

---
+++ string.php
@@ -0,0 +1,8 @@
+<?php  
+$num1=10;   
+$str1='trying variable $num1';  
+$str2='trying backslash n and backslash t inside single quoted string \n \t';  
+$str3='Using single quote \'my quote\' and \\backslash';  
+$str4= "Using double quote \"my quote\" and \\backslash";
+echo "$str1 <br/> $str2 <br/> $str3 <br /> $str4";  
+?>  
