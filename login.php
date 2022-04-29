1e3d1bd63d816df12a8bf1fa071441d85762f34a
Ankush <ankush357159@gmail.com>
2022-04-29 13:44:30 +0530

    form in php

---
+++ login.php
@@ -0,0 +1,10 @@
+<?php 
+// $name = $_GET['name'];
+// echo "Welcome, " . $name;
+
+$name = $_POST['name'];
+$password = $_POST['password'];
+
+echo "Welcome: $name, your password is: $password";
+
+?>
